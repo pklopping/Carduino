d81399d5594ca2ea4fc7ca8408d93567a4c4c700
Paul Klopping <au70pi1337@gmail.com>
2015-01-25 21:00:49 -0600

    Testing the arduino mega with the LED strips

--- sketch_jan21a/sketch_jan21a.ino
+++ sketch_jan21a/sketch_jan21a.ino
@@ -2,12 +2,12 @@
 #include <DallasTemperature.h>
  
 // Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 24
 
-int redPin = 9;
-int greenPin = 10;
-int bluePin = 11;
-int switchPin = 3;
+int redPin = 2;
+int greenPin = 3;
+int bluePin = 7;
+int switchPin = 47;
 int switchState = 0;
 float currentTemp = 0;
 float cold = 73;
@@ -23,8 +23,8 @@ DallasTemperature sensors(&oneWire);
 void setup(void)
 {
   // start serial port
-  //Serial.begin(9600);
-  //Serial.println("Dallas Temperature IC Control Library Demo");
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
 
   // Start up the library
   sensors.begin();
@@ -36,13 +36,13 @@ void loop(void)
 {
   switchState = digitalRead(switchPin);
   if (switchState == HIGH) {
-    //Serial.print(" Requesting temperatures...");
+    Serial.print(" Requesting temperatures...");
     sensors.requestTemperatures(); // Send the command to get temperatures
-    //Serial.println("DONE");
-    //Serial.print("Temperature for Device 1 is: ");
+    Serial.println("DONE");
+    Serial.print("Temperature for Device 1 is: ");
     currentTemp = sensors.getTempFByIndex(0);
   
-    //Serial.print(currentTemp); // Why "byIndex"? 
+    Serial.println(currentTemp); // Why "byIndex"? 
     currentTemp = max(currentTemp, cold);
     currentTemp = min(currentTemp, hot);
     int currentColor = map(currentTemp, cold, hot, 0, 255);

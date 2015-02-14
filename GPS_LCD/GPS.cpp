4ca20a9df5dfc22d72a9841616a4628cccf68226
Paul Klopping <=>
2015-02-14 13:57:14 -0600

    Can now save up to 1000 log files.
    Who would ever need more than 1000 files? (it's like having more than 64K of memory)

--- GPS_LCD/GPS.cpp
+++ GPS_LCD/GPS.cpp
@@ -37,10 +37,11 @@ int GPS::setupSD() {
 }
 
 int GPS::createLog() {
-	strcpy(buffer, "GPSLOG00.TXT");
+	strcpy(buffer, "LOG000.TXT");
 	for (int8_t i = 0; i < 1000; i++) {
-		buffer[6] = '0' + i/10;
-		buffer[7] = '0' + i%10;
+		buffer[3] = '0' + (uint8_t)floor(i/100);
+		buffer[4] = '0' + (uint8_t)floor(i/100)%10;
+		buffer[5] = '0' + i%10;
 		// create if does not exist, do not open existing, write, sync after write
 		if (! SD.exists(buffer)) {
 			break;
